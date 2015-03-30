67ccca3f3381f90d84f701f44e8c186484e62dbc
JLStack <stackjl@appstate.edu>
2015-03-29 20:40:19 -0400

    added date and time to RegisterSnipe.py

--- RegisterSnipe.py
+++ RegisterSnipe.py
@@ -1,15 +1,22 @@
 __author__ = 'lukestack'
 from selenium import webdriver
 import time
+from datetime import datetime
 
 USERNAME = ''
 PASSWORD = ''
 REGISTERPIN = ''
-
+SEMESTER = 'Fall 2015'
+CRNS = ["11156"] #max 10 CRNS
+REGISTERDATE = datetime(2015, 3, 29, 20, 12)#datetime('Year', 'Month', 'Date', 'Hour(0-24)', 'Minute')
+present = datetime.now()
+while present.date() < REGISTERDATE.date():
+    pass
+while present.time() < REGISTERDATE.time():
+    present = datetime.now()
 try:
     path_to_chromedriver = '/Users/lukestack/Downloads/chromedriver' # change path as needed
     browser = webdriver.Chrome(executable_path = path_to_chromedriver)
-
     url = 'https://portal4.appstate.edu/cp/home/displaylogin'
     browser.get(url)
     browser.find_element_by_xpath("//*[@id='leftcol']/fieldset/form[1]/span/input").send_keys(USERNAME)
@@ -22,14 +29,15 @@ try:
     browser.find_element_by_xpath("//a[contains(text(),'Registration')]").click()
     browser.find_element_by_xpath("//a[contains(text(),'Add/Drop')]").click()
     browser.find_element_by_id("term_id").click()
-    browser.find_element_by_xpath("//*[@id='term_id']/option[1]").click()
+    browser.find_element_by_xpath("//*[@id='term_id']/option[contains(text(),'" + SEMESTER + "')]").click()
     browser.find_element_by_xpath("//input").click()
     browser.find_element_by_xpath("//input").send_keys(REGISTERPIN)
     browser.find_element_by_xpath("//input[contains(@type, 'submit')]").click()
-    """
-    for result in results:
-        print result.text
-    """
+    crn_inputs = browser.find_elements_by_xpath("//input[contains(@id, 'crn_id')]")
+    print len(crn_inputs)
+    for i in range(len(CRNS)):
+        crn_inputs[i].send_keys(CRNS[i])
+    browser.find_element_by_xpath("/html/body/div[3]/form/input[contains(@value, 'Submit Changes')]").click()
 except:
     pass
 time.sleep(5)
